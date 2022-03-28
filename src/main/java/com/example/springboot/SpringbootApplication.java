131078a046650a7984922b4431596c6f57947942
lxz <928245115@qq.com>
2022-03-28 21:45:39 +0800

    rot

--- src/main/java/com/example/springboot/SpringbootApplication.java
+++ src/main/java/com/example/springboot/SpringbootApplication.java
@@ -11,7 +11,7 @@ public class SpringbootApplication {
 	public static void main(String[] args) {
 
 		SpringApplication.run(SpringbootApplication.class, args);
-		System.out.println("hahaaaa");
+		System.out.println("嘻嘻嘻");
 	}
 
 }
