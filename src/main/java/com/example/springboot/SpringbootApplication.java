04a780b2a2b0891a44c5ad64e6fd7d7af6aeaa66
lxz <928245115@qq.com>
2022-03-28 21:00:00 +0800

    aa

---
+++ src/main/java/com/example/springboot/SpringbootApplication.java
@@ -0,0 +1,17 @@
+package com.example.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringBootVersion;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.oas.annotations.EnableOpenApi;
+
+@SpringBootApplication
+public class SpringbootApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(SpringbootApplication.class, args);
+
+	}
+
+}
